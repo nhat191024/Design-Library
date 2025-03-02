d6d63cf9f577385181cea7c445d789a5f3651eab
Catty <ducvu73@gmail.com>
2025-03-02 14:12:44 +0700

    feat: add database migrations for users, categories, products, images, tags, and tag_products

---
+++ database/migrations/2025_03_02_063220_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id(); // Auto-incrementing primary key
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('category_id'); // Foreign key to category
+            $table->timestamps();
+
+            // Foreign Key Constraints
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
