c19552293f58fbd116df6e05419b77cc4c6224ef
Catty <ducvu73@gmail.com>
2025-03-03 11:36:12 +0700

    feat: add footer and navigation partials for client views

---
+++ resources/views/client/partials/navigation.blade.php
@@ -0,0 +1,58 @@
+<div class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+        <!-- Mobile dropdown -->
+        <div class="dropdown">
+            <label tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </label>
+            <ul tabindex="0"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow">
+                <li><a href="/">Trang chủ</a></li>
+                <li><a href="/assets/tai-nguyen-khac">File Mới</a></li>
+                <li><a href="/assets/plugin">Plugin</a></li>
+                <li><a href="/assets/video-edit">Video</a></li>
+                <li><a href="/assets/brush">Brush</a></li>
+                <li><a href="/assets/effect">Effect</a></li>
+                <li><a href="/assets/chu-3d">Chữ 3D</a></li>
+                <li><a href="/assets/san-pham-v">VIP</a></li>
+                <li><a href="https://s99.vn/assets/font">Font</a></li>
+                <li><a href="https://s99.vn/assets/viet-nam">Việt Nam</a></li>
+                <li><a href="/category">Xem thêm</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/topup-plan">VIP tải không giới hạn</a></li>
+                <li><a href="/signup?callback=/">Đăng ký</a></li>
+                <li><a href="/login?callback=/">Đăng nhập</a></li>
+            </ul>
+        </div>
+        <!-- Logo -->
+        <a class="btn btn-ghost" href="/">
+            <img src="/static/logo.png" alt="S99" width="40" height="30">
+        </a>
+    </div>
+
+    <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+            <li><a href="/">Trang chủ</a></li>
+            <li><a href="/assets/tai-nguyen-khac">File Mới</a></li>
+            <li><a href="/assets/plugin">Plugin</a></li>
+            <li><a href="/assets/video-edit">Video</a></li>
+            <li><a href="/assets/brush">Brush</a></li>
+            <li><a href="/assets/effect">Effect</a></li>
+            <li><a href="/assets/chu-3d">Chữ 3D</a></li>
+            <li><a href="/assets/san-pham-v">VIP</a></li>
+            <li><a href="https://s99.vn/assets/font">Font</a></li>
+            <li><a href="https://s99.vn/assets/viet-nam">Việt Nam</a></li>
+            <li><a href="/category">Xem thêm</a></li>
+        </ul>
+    </div>
+
+    <div class="navbar-end">
+        <ul class="menu menu-horizontal px-1">
+            <li><a href="/blog">Liên hệ</a></li>
+        </ul>
+    </div>
+</div>
