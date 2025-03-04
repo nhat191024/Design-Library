684e741485eb45bd730e5746a6df2ce5d1c01358
nhat191024 <richberchannel01@gmail.com>
2025-03-04 18:47:07 +0700

    feat: refactor design index view to use AdminLayout and integrate DataTables

--- resources/views/admin/design/index.blade.php
+++ resources/views/admin/design/index.blade.php
@@ -1,18 +1,48 @@
-@extends('layouts.admin.app')
-@section('header')
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        {{ __('Designs') }}
-    </h2>
-@endsection
+<x-admin-layout>
+    <x-slot name="style">
+        <style>
+            #design-table th {
+                text-align: center;
+            }
 
-@section('content')
+            #design-table td {
+                text-align: center;
+            }
+        </style>
+    </x-slot>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Designs') }}
+        </h2>
+    </x-slot>
+
+    @include('admin.design.partials.edit-design-form')
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class=" max-w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    @include('admin.design.partials.datatables')
                 </div>
             </div>
         </div>
     </div>
-@endsection
+
+    <x-slot name="script">
+        <script>
+            $(document).ready(function() {
+                $('#design-table').DataTable({
+                    language: {
+                        paginate: {
+                            "first": "",
+                            "last": "",
+                            "next": "Next",
+                            "previous": "Previous"
+                        },
+                    }
+                });
+            });
+        </script>
+    </x-slot>
+</x-admin-layout>
