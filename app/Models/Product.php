6f8f32d91caa89ed497f33901ee554b488a5bd77
Catty <ducvu73@gmail.com>
2025-03-05 21:53:50 +0700

    feat: implement slug generation for products on create and update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -27,5 +28,34 @@ class Product extends Model
     public function Tags()
     {
         return $this->belongsToMany(Tag::class, 'tag_products', 'product_id', 'tag_id');
+    }
+
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->slug = $product->slug ?: self::generateSlug($product);
+        });
+
+        static::updating(function ($product) {
+            $product->slug = self::generateSlug($product);
+        });
+    }
+
+    /**
+     * Generate a slug for the given product
+     *
+     * @param  self  $product
+     * @return string
+     */
+    public static function generateSlug($product)
+    {
+        $timestamp = time();
+        if (preg_match('/[^\x{0000}-\x{007F}]+/u', $product->name)) {
+            return 'san-pham-' . Str::slug($product->name) . '-' . $timestamp . '-' . ($product->id ?? Str::random(6));
+        }
+        return Str::slug($product->name) . '-' . ($product->id ?? $timestamp);
     }
 }
