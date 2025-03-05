e887a24af4ab90a20ba611eb45ca9c4a6b942768
Catty <ducvu73@gmail.com>
2025-03-05 21:54:11 +0700

    feat: update routes to include product detail view with slug-based routing

--- routes/client.php
+++ routes/client.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Client\HomeController;
+use App\Http\Controllers\Client\ShopController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('profile.edit');
+Route::get('/', [HomeController::class, 'index'])->name('client.profile.edit');
+Route::get('/products/detail/{slug}', [ShopController::class, 'detail'])->name('client.product.detail');
