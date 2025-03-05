0270205ae6390745db4bdc2864d155311995316b
nhat191024 <richberchannel01@gmail.com>
2025-03-05 15:30:15 +0700

    feat: integrate Select2 for enhanced select input styling and functionality

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -18,6 +18,11 @@
         rel="stylesheet" integrity="sha384-uMRVFAehEmeRx+eu65ZAwUtvyFbGSAXOA+y0/bktyqsYwlw8575VE7T7o5PqC9HY"
         crossorigin="anonymous">
 
+    <!-- select picker css -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/gh/erimicel/select2-tailwindcss-theme/dist/select2-tailwindcss-theme-plain.min.css">
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -47,14 +52,35 @@
     </div>
 </body>
 
-{{-- datatables script --}}
+<!-- datatables script -->
 <script src="https://cdn.datatables.net/v/dt/jq-3.7.0/dt-2.2.2/r-3.0.4/sp-2.3.3/sr-1.4.1/datatables.min.js"
     integrity="sha384-EyOrkIw2BJ0wGDDncNwhfC5UwkD+tjKPyPNUqOd9J92FC+Y3JT5Q/32Ad5/x0ylC" crossorigin="anonymous">
 </script>
+
+<!-- select picker -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <!-- script -->
 @isset($script)
     {{ $script }}
 @endisset
+
+<script>
+    $(function() {
+        $('select').each(function() {
+            let options = {
+                theme: 'tailwindcss-3',
+                width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-full') ?
+                    '100%' : 'style',
+                placeholder: $(this).data('placeholder') || 'Select an option',
+                allowClear: Boolean($(this).data('allow-clear')),
+                closeOnSelect: !$(this).attr('multiple'),
+                tags: Boolean($(this).data('tags')),
+            };
+
+            $(this).select2(options);
+        });
+    });
+</script>
 
 </html>
