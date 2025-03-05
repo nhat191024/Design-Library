b72789c4c921187d289ccffaf89d272260f14269
nhat191024 <richberchannel01@gmail.com>
2025-03-05 16:08:08 +0700

    feat: enhance design table layout and update image display with dynamic data

--- resources/views/admin/design/partials/datatables.blade.php
+++ resources/views/admin/design/partials/datatables.blade.php
@@ -1,45 +1,54 @@
 <section>
-    <button class="btn btn-success float-end mb-5">Add design</button>
-    <table id="design-table" class="display order-column">
-        <thead>
-            <tr>
-                <th>STT</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>First Image</th>
-                <th>Category</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($designs as $key => $design)
-                <tr>
-                    <td>{{ ++$key }}</td>
-                    <td>{{ $design['name'] }}</td>
-                    <td>{{ $design['description'] }}</td>
-                    <td>
-                        <img class="w-24 h-24 object-cover mx-auto"
-                            src="https://www.shutterstock.com/editorial/image-editorial/M6z1QcweO7DdU3w4Mjg4Mg==/cat-that-looks-like-hitler-window-dublin-440nw-3387833a.jpg"
-                            alt="">
-                    </td>
-                    <td>{{ $design['category_id'] }}</td>
-                    <td>
-                        <button class="btn btn-sm btn-primary" onclick="my_modal_5.showModal()">Edit</button>
-                        <a href="" class="btn btn-sm btn-error">Delete</a>
-                        <a href="" class="btn btn-sm btn-info">All Img</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>STT</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>First Image</th>
-                <th>Category</th>
-                <th>Action</th>
-            </tr>
-        </tfoot>
-    </table>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <button class="btn btn-success float-end mb-5">Add design</button>
+                    <table id="design-table" class="display order-column">
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>First Image</th>
+                                <th>Category</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($designs as $key => $design)
+                                <tr>
+                                    <td>{{ ++$key }}</td>
+                                    <td>{{ $design->name }}</td>
+                                    <td>{{ $design->description }}</td>
+                                    <td>
+                                        <img class="w-24 h-24 object-cover mx-auto"
+                                            src="{{ asset($design->images->first()->url) }}" alt="">
+                                    </td>
+                                    <td>{{ $design->category->name }}</td>
+                                    <td>
+                                        <a href="{{ route('designs.edit', $design->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                        <a href="" class="btn btn-sm btn-error">Delete</a>
+                                        <a href="" class="btn btn-sm btn-info">All Img</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>STT</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>First Image</th>
+                                <th>Category</th>
+                                <th>Action</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
