93e34895291e9529b77c8faf183ce4263a5452f6
Catty <ducvu73@gmail.com>
2025-03-05 21:53:42 +0700

    feat: refactor home view title to use a class constant for consistency

--- app/Http/Controllers/client/HomeController.php
+++ app/Http/Controllers/client/HomeController.php
@@ -10,18 +10,15 @@ use App\Models\Tag;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $PAGE_TITLE = "Design Showcase";
+    
     public function index()
     {
         $tags = Tag::all();
         $categories = Category::all();
         $products = Product::with('Category', 'Images', 'Tags')->get();
         return view('client.home.home')->with([
-            'title' => "Design Showcase",
+            'title' => $this->PAGE_TITLE,
             'tags' => $tags,
             'categories' => $categories,
             'products' => $products
