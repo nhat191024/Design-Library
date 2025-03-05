67b6465379982e712878b966280e65fa37a91fa4
Catty <ducvu73@gmail.com>
2025-03-06 06:11:00 +0700

    feat: add product routes for shop index, search, and category filtering

--- routes/client.php
+++ routes/client.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\ShopController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('client.profile.edit');
-Route::get('/products/detail/{slug}', [ShopController::class, 'detail'])->name('client.product.detail');
+Route::get('/products', [ShopController::class, 'index'])->name('client.shop.index'); // shop index
+Route::get('/products?q={query?}', [ShopController::class, 'search'])->name('client.shop.search'); // shop search
+Route::get('/products/category/{slug}', [ShopController::class, 'category'])->name('client.shop.category'); // shop category filter
+Route::get('/products/detail/{slug}', [ShopController::class, 'detail'])->name('client.product.detail'); // product detail
