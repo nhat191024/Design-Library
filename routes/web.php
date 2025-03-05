2f5de0f91cf432bd774b95b084444ff2e4816146
nhat191024 <richberchannel01@gmail.com>
2025-03-05 17:43:34 +0700

    feat: add GET route for design item deletion with confirmation

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/designs/{id}', [DesignController::class, 'showEditForm'])->name('designs.edit');
     Route::patch('/designs/update/{id}', [DesignController::class, 'update'])->name('designs.update');
     Route::post('/designs/upload-image', [DesignController::class, 'uploadImage'])->name('designs.upload-image');
-    Route::delete('/designs/{id}', [DesignController::class, 'destroy'])->name('designs.destroy');
     Route::delete('/designs/images/{image}', [DesignController::class, 'deleteImage'])->name('designs.delete-image');
+    Route::get('/designs/delete/{id}', [DesignController::class, 'destroy'])->name('designs.destroy');
 });
 
 require __DIR__.'/auth.php';
