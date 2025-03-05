10490ae4fa7e8bfbc6fad9763daa08677bb59f31
Catty <ducvu73@gmail.com>
2025-03-06 06:11:19 +0700

    feat: implement automatic slug generation for categories on create and update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -28,5 +29,26 @@ class Category extends Model
     public function Products()
     {
         return $this->hasMany(Product::class, 'category_id');
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            $category->slug = $category->slug ?: self::generateSlug($category);
+        });
+
+        static::updating(function ($category) {
+            $category->slug = self::generateSlug($category);
+        });
+    }
+    public static function generateSlug($category)
+    {
+        $timestamp = time();
+        if (preg_match('/[^\x{0000}-\x{007F}]+/u', $category->name)) {
+            return 'danh-muc-' . Str::slug($category->name) . '-' . $timestamp . '-' . ($category->id ?? Str::random(6));
+        }
+        return Str::slug($category->name) . '-' . ($category->id ?? $timestamp);
     }
 }
