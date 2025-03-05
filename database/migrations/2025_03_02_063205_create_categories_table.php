6c57a236ac6383ce1a2f7823e2fcd0c34d7d1656
nhat191024 <richberchannel01@gmail.com>
2025-03-05 22:05:24 +0700

    feat: add image column to categories table for enhanced category data

--- database/migrations/2025_03_02_063205_create_categories_table.php
+++ database/migrations/2025_03_02_063205_create_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->boolean('is_show')->default(1);
+            $table->string('image')->nullable();
 
             $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
 
