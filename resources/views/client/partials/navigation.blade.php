da79b8132bb1950de685c1d1b5b2aae92311ee38
Catty <ducvu73@gmail.com>
2025-03-09 06:58:33 +0700

    feat: add theme toggle functionality with localStorage support

--- resources/views/client/partials/navigation.blade.php
+++ resources/views/client/partials/navigation.blade.php
@@ -33,8 +33,29 @@
     </div>
 
     <div class="navbar-end">
+        <label class="toggle text-base-content">
+            <input type="checkbox" value="dark" class="theme-controller hidden">
+
+            <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
+
+            <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
+
+        </label>
         <ul class="menu menu-horizontal px-1 text-lg font-bold">
             <li><a href="{{ route('client.contact.index') }}">LIÊN HỆ</a></li>
         </ul>
     </div>
 </div>
+<script>
+    $(document).ready(function () {
+        const themeToggle = $(".theme-controller");
+        let currentTheme = localStorage.getItem("theme") || "light";
+        $("html").attr("data-theme", currentTheme);
+        themeToggle.prop("checked", currentTheme === "dark");
+        themeToggle.on("change", function () {
+            const newTheme = themeToggle.is(":checked") ? "dark" : "light";
+            $("html").attr("data-theme", newTheme);
+            localStorage.setItem("theme", newTheme);
+        });
+    });
+</script>
