353f17a35acd697eee8b73a57831f15004683fae
Catty <ducvu73@gmail.com>
2025-03-09 06:58:24 +0700

    feat: add script to fix UI flickering by setting theme from localStorage

--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -8,7 +8,14 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title }}</title>
-    <!-- Scripts -->
+
+    <!-- Fix UI flickering script -->
+    <script>
+        (function () {
+            let theme = localStorage.getItem("theme") || "light";
+            document.documentElement.setAttribute("data-theme", theme);
+        })();
+    </script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
