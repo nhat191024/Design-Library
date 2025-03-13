aca07b1ddd2140c29e1d8ec2a98c9c96ba474a73
Catty <ducvu73@gmail.com>
2025-03-14 06:06:00 +0700

    feat: replace social media icons with updated images for Zalo and Facebook

--- resources/views/client/shop/detail.blade.php
+++ resources/views/client/shop/detail.blade.php
@@ -90,12 +90,10 @@
             {{-- Social Share --}}
             <div class="flex gap-2 mt-4" onclick="location.href='{{ route('client.contact.index') }}'">
                 <button class="btn btn-circle btn-ghost">
-                    <img src="{{ asset('images/icons/zalo-light.png') }}" alt="Zalo Icon" class="h-8 w-8 text-primary">
+                    <img src="{{ asset('images/logos/zalo.png') }}" alt="Zalo Icon" class="h-6 w-6 text-primary">
                 </button>
                 <button class="btn btn-circle btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-blue-600">
-                        <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                    </svg>
+                    <img src="{{ asset('images/logos/facebook.png') }}" alt="Facebook Icon" class="h-6 w-6 text-primary">
                 </button>
             </div>
         </div>
