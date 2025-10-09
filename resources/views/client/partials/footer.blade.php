c095764c457ddc85588b48876e156b2afa7eb0b0
Fumino The Fish <richberchannel01@gmail.com>
2025-10-09 11:10:29 +0700

    fix: improve HTML structure and readability in footer partial

--- resources/views/client/partials/footer.blade.php
+++ resources/views/client/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer sm:footer-horizontal bg-base-300 text-base-content p-10">
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full gap-8 container mx-auto">
+<footer class="footer bg-base-300 p-10 text-base-content sm:footer-horizontal">
+    <div class="container mx-auto grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
         <nav class="flex flex-col">
             <h6 class="footer-title">Về chúng tôi</h6>
             <p class="link link-hover">Thietkedecor.vn là đối tác hàng đầu của các doanh nghiệp, đơn vị tổ chức sự kiện.
@@ -12,21 +12,20 @@
         <nav class="flex flex-col">
             <h6 class="footer-title">Phương thức liên hệ</h6>
             <div class="flex flex-col">
-                <a href="{{ route('client.contact.index') }}" class="link link-hover">Zalo/Call: 0908556913 (Minh)</a>
-                <a href="{{ route('client.contact.index') }}" class="link link-hover">Zalo/Call: 0975038534 (Phương)</a>
+                <a class="link link-hover" href="{{ route('client.contact.index') }}">Zalo/Call: 0908556913 (Minh)</a>
+                <a class="link link-hover" href="{{ route('client.contact.index') }}">Zalo/Call: 0975038534 (Phương)</a>
             </div>
             <h6 class="footer-title mt-4">Cộng đồng</h6>
             <div class="grid grid-flow-col gap-4">
-                <a href="{{ route('client.contact.index') }}" class="link link-hover">Facebook</a>
-                <a href="{{ route('client.contact.index') }}" class="link link-hover">Zalo</a>
-                <a href="{{ route('client.contact.index') }}" class="link link-hover">Pinterest</a>
+                <a class="link link-hover" href="{{ route('client.contact.index') }}">Facebook</a>
+                <a class="link link-hover" href="{{ route('client.contact.index') }}">Zalo</a>
+                <a class="link link-hover" href="{{ route('client.contact.index') }}">Pinterest</a>
             </div>
         </nav>
         <nav class="flex flex-col">
             <h6 class="footer-title">Danh mục thiết kế</h6>
             @foreach ($shared_categories->take(8) as $shared_category)
-                <a href="{{ route('client.shop.category', ['slug' => $shared_category->slug]) }}"
-                    class="link link-hover">- {{ $shared_category->name }}</a>
+                <a class="link link-hover" href="{{ route('client.shop.category', ['slug' => $shared_category->slug]) }}">- {{ $shared_category->name }}</a>
             @endforeach
         </nav>
         <nav class="flex flex-col">
