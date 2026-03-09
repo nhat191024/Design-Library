7c06adfebc57a2dd99c9ec03c2a47fb8d9d680c0
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-03-09 14:46:51 +0700

    feat: add initial Docker setup with configuration files and entrypoint script

---
+++ docker-entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+echo "Starting Laravel application setup..."
+
+# Wait for mounted volumes to be available
+sleep 2
+
+# Generate application key
+echo "Generating application key..."
+php artisan key:generate --force
+
+# run migrations
+# php artisan migrate --force
+
+# run seeders
+# php artisan db:seed --force
+
+# run storage link
+# php artisan storage:link
+
+# run npm build
+# echo "building assets..."
+# npm run build:ssr
+
+# Set proper permissions
+echo "Setting permissions..."
+chgrp -R www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/public
+chmod -R g+w /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/public
+find /var/www/html/storage -type d -exec chmod g+s {} +
+find /var/www/html/bootstrap/cache -type d -exec chmod g+s {} +
+
+# Clear and cache configurations
+echo "Clearing and caching configurations..."
+php artisan optimize:clear
+
+# Cache for production
+echo "Caching for production..."
+# php artisan route:cache
+# php artisan view:cache
+# php artisan event:cache
+php artisan optimize
+# php artisan filament:optimize
+
+# fix nginx tmp dir permission
+echo "Fixing nginx client_body_temp_path permissions..."
+mkdir -p /var/lib/nginx/tmp/client_body
+chown -R www-data:www-data /var/lib/nginx/
+chmod -R 755 /var/lib/nginx/
+
+echo "Laravel application setup completed!"
+
+# Start Apache
+exec apache2-foreground
+
+# Start Supervisord
+# exec /usr/bin/supervisord -c /etc/supervisor/conf.d/laravel.conf
